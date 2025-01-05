df9baa5af3f7520f74735e737479ec7f677f4c6a
Alex Cowan <alexcowan@Mac.modem>
2025-01-05 21:20:00 +1100

    Added Telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		-- Your keymaps go here
+		vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
+		vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
+		vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
+		vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {})
+	end,
+}
