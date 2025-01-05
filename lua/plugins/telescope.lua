3453307971586da92ca60fd0086b8dd9659a6704
Alex Cowan <alexcowan@Mac.modem>
2025-01-05 21:42:07 +1100

    Added Lazygit, UndoTele, and telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,10 +3,22 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		-- Your keymaps go here
-		vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
-		vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
-		vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
-		vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {})
+		-- Require telescope and builtin at the start
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+		-- The actual keymaps
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word" })
+		vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Marks" })
+		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent Files" })
+		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
+		vim.keymap.set("n", "<leader>fs", builtin.git_status, { desc = "Git Status" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
+
+		-- I removed the projects mapping as it requires additional setup
 	end,
 }
